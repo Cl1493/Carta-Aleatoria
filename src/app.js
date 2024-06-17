9fb5dbcf4bbd2d9aba0249a52a50c5517133ca51
Carla <152617155+Cl1493@users.noreply.github.com>
2024-06-17 09:34:03 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+// import "./index.html";
+
+window.onload = function() {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+};
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let card = document.querySelector(".card");
+  let removeClassName = card.classList["1"];
+  document.querySelector(".card").classList.remove(removeClassName);
+  card.classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+});
+let generateRandomNum = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
